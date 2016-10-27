e9496db136523d7999627afc4f50898cc8a8f0b2
Levindo Neto <levindo.neto@inf.ufrgs.br>
2016-10-27 13:30:59 -0200

    ADD: Libraries and parcial defines of the simulator

---
+++ cachesim.cpp
@@ -0,0 +1,16 @@
+/* Cache Simulator, by Levindo Neto */
+/* Build environment: Ubuntu 14.04, with g++ */
+#include<cstdio>  
+#include<cstdlib>
+#include<cstring>
+#include<cmath>
+#include<stack>
+#include<string>
+#include<iostream>
+using namespace std;
+
+#define Bytes_Per_Line 32
+#define Normal_Access_Cycles 1
+#define Num_Sets 64
+#define Overhead_Bits 25
+
