7d78684b7a86bda4c774e58d21defe1860f0a109
levindoneto <levindo.neto@inf.ufrgs.br>
2016-10-28 00:51:26 -0200

    ADD: Variables from cache description, MODIFY: -Wall to the gcc of Makefile

--- cache_simulator.c
+++ cache_simulator.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 int main()  
-{      
-    printf("Cache Simulator\n");
-    
-    file_specs_cache = fopen("/files/cachedesc.bin", "rb");
-
-
-    fclose(file_specs_cache);
-
+{   
+    int line_size;
+    int number_of_lines;
+    int associativity;
+    char replacement_policy[5]; // LRY or FIFO + \0
+    char write_policy[13]; // WRITEBACK or WRITETHROUGH + \0
    
+    printf("\n*** Cache Simulator ***\n");
+    
+    FILE *ptr_file_specs_cache;
+    ptr_file_specs_cache = fopen("cachedesc.bin", "rb");
+    
+    if(!ptr_file_specs_cache) {
+        printf("This file is unable to open!");
+        return -1;
+    }
+
+    fclose(ptr_file_specs_cache);
+
    return 0;
 }  
