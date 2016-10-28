9b88ec9446f14a1584a7c0ccfaa32578a42e0f22
levindoneto <levindo.neto@inf.ufrgs.br>
2016-10-28 00:09:11 -0200

    ADD: File of cache description to the cache simulator

--- cache_simulator.c
+++ cache_simulator.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 int main()  
 {      
-   printf("Cache Simulator\n");     
+    printf("Cache Simulator\n");
+    
+    file_specs_cache = fopen("/files/cachedesc.bin", "rb");
+
+
+    fclose(file_specs_cache);
+
+   
    return 0;
 }  
