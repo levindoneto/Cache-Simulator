9fc4bb74234e49604cf2ae3b414515803a5ac0c3
levindoneto <levindo.neto@inf.ufrgs.br>
2016-11-02 13:03:10 -0200

    ADD: Header file for the cache simulator

---
+++ cache_simulator.h
@@ -0,0 +1,20 @@
+// Libraries
+#include <stdio.h>
+#include "cache_simulator.h"
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct results {
+    int acess_count;
+    int read_hits;
+    int read_misses;
+    int write_hits;
+    int write_misses;
+} Results;
+
+typedef struct desc {
+    int line_size;               // Block size
+    int number_of_lines;         // Number of blocks
+    int associativity;           // Number of blocks per set
+    char replacement_policy[5];  // LRY or FIFO + \0
+} Desc;
